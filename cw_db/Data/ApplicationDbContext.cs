8e95e8e54fcf9887a89eebfb8b5ea46632493b29
DESKTOP-8LFJ8H8\Nox <eriks.aire.07@gmail.com>
2022-12-07 21:20:41 +0200

    Changed Db Context to Support 1:m relationships

--- cw_db/Data/ApplicationDbContext.cs
+++ cw_db/Data/ApplicationDbContext.cs
@@ -11,6 +11,34 @@ namespace cw_db.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Order>()
+                .HasOne<Customer>(s=>s.customer)
+                .WithMany(g=>g.Orders)
+                .HasForeignKey(s=>s.CustomerId);
+            
+            builder.Entity<Order>()
+                .HasOne<Supplier>(s => s.Supplier)
+                .WithMany(g => g.Orders)
+                .HasForeignKey(s => s.SupplierId);
+
+            builder.Entity<Feedback>()
+                .HasOne<Customer>(s => s.customer)
+                .WithMany(g => g.feedBacks)
+                .HasForeignKey(s => s.CustomerId);
+
+
+
+
+            builder.Entity<Order>()
+                .HasMany<Product>(s => s.Products)
+                .WithMany(o => o.Orders)
+                .UsingEntity(j => j.ToTable("OrderProduct"));
+
+        }
         public DbSet<Product> Products { get; set; }
         public DbSet<Supplier> Suppliers{ get; set; }
         public DbSet<Customer> Customers{ get; set; }
---
+++ cw_db/Migrations/20221207183742_ConnectionMigration.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cw_db.Migrations
+{
+    public partial class ConnectionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Feedbacks_AspNetUsers_CustomerId",
+                table: "Feedbacks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Suppliers_SupplierId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SupplierId",
+                table: "Orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerId",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerId",
+                table: "Feedbacks",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Feedbacks_AspNetUsers_CustomerId",
+                table: "Feedbacks",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Suppliers_SupplierId",
+                table: "Orders",
+                column: "SupplierId",
+                principalTable: "Suppliers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Feedbacks_AspNetUsers_CustomerId",
+                table: "Feedbacks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Suppliers_SupplierId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SupplierId",
+                table: "Orders",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerId",
+                table: "Orders",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerId",
+                table: "Feedbacks",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Feedbacks_AspNetUsers_CustomerId",
+                table: "Feedbacks",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Suppliers_SupplierId",
+                table: "Orders",
+                column: "SupplierId",
+                principalTable: "Suppliers",
+                principalColumn: "Id");
+        }
+    }
+}
